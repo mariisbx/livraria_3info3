c2402052a8a6284cdc38aaaf2df17b2ee11a694f
mariisbx <mariaaparecida.ifc.araquari@gmail.com>
2026-04-28 08:21:04 -0300

    Fix: Arrumando bad-request

--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (
@@ -22,6 +24,8 @@ router.register(r'editoras', EditoraViewSet, basename='editoras')
 router.register(r'livros', LivroViewSets, basename='livros')
 router.register(r'usuarios', UserViewSet, basename='usuarios')
 
+from uploader.router import router as uploader_router
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     # OpenAPI 3
@@ -44,4 +48,6 @@ urlpatterns = [
     path('api/registro/', UserRegistrationView.as_view(), name='user_registration'),
     # API
     path('api/', include(router.urls)),
+    path('api/media/', include(uploader_router.urls)),
 ]
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
